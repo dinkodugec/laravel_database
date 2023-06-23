56c5d6db3bc879e43503eacdee91ad1ca5383613
Dinko <dinko.dugec@gmail.com>
2023-06-23 17:01:56 +0200

    using json data to seed db from json encode

--- database/seeders/PostsTableSeeder.php
+++ database/seeders/PostsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class PostsTableSeeder extends Seeder
 {
@@ -13,7 +14,7 @@ class PostsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $posts = collect([
+  /*       $posts = collect([
             [
                 'title' => 'Post One',
                 'slug' => 'post-one',
@@ -30,10 +31,20 @@ class PostsTableSeeder extends Seeder
                 'is_published' => true,
                 'min_to_read' => 2,
             ]
-        ]);
+        ]); */
+
+        $json = File::get('database/json/posts.json');
+        $posts = collect(json_decode($json));
 
         $posts->each(function ($post) {
-            Post::create($post);
+            Post::insert([
+                "title" => $post->title,
+                "slug" => $post->slug,
+                "excerpt" => $post->excerpt,
+                "description" => $post->description,
+                "is_published" => $post->is_published,
+                "min_to_read" => $post->min_to_read
+            ]);
         });
     }
 }
