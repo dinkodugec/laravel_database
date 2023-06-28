e65d84bd59e1263fbf9928eb488870aa3037337e
Dinko <dinko.dugec@gmail.com>
2023-06-28 22:58:55 +0200

    insert in db; insert insertOrIgnore. upsert

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -65,7 +65,60 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        DB::table('posts')->insert([
+            [
+                'user_id' => 1,
+                'title' => 'Insert through the DB facade 2',
+                'slug' => 'insert-through-the-db-facade-2',
+                'excerpt' => 'excerpt',
+                'description' => 'description',
+                'is_published' => true,
+                'min_to_read' => 2,
+            ],
+            [
+                'user_id' => 1,
+                'title' => 'Insert through the DB facade 3',
+                'slug' => 'insert-through-the-db-facade-3',
+                'excerpt' => 'excerpt',
+                'description' => 'description',
+                'is_published' => true,
+                'min_to_read' => 2,
+            ]
+        ]);
+
+        DB::table('posts')->insertOrIgnore([
+            [
+                'user_id' => 1,
+                'title' => 'X',
+                'slug' => 'x',
+                'excerpt' => 'excerpt',
+                'description' => 'description',
+                'is_published' => true,
+                'min_to_read' => 2,
+            ],
+            [
+                'user_id' => 1,
+                'title' => 'Insert through the DB facade 3',
+                'slug' => 'insert-through-the-db-facade-3',
+                'excerpt' => 'excerpt',
+                'description' => 'description',
+                'is_published' => true,
+                'min_to_read' => 2,
+            ]
+        ]);
+
+        // upsert() insert or update a record in database
+        $posts = DB::table('posts')->upsert([
+            [
+                'user_id' => 1,
+                'title' => 'X',
+                'slug' => 'x',
+                'excerpt' => 'Updated excerpt',
+                'description' => 'Updated description',
+                'is_published' => true,
+                'min_to_read' => 2,
+            ],
+        ], ['title', 'slug']);  
     }
 
     /**
