c8b63f7d3d74cd0df53614ebe8367be6a3888873
Dinko <dinko.dugec@gmail.com>
2023-06-28 23:48:03 +0200

    check if exists or not exists

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,16 +12,19 @@ class PostController extends Controller
      */
     public function index()
     {   
-        // whereNot()
-        DB::table('posts')->whereNot('min_to_read', 1)->get();
-        DB::table('posts')->whereNot('min_to_read', '>', 1)->get();
-        
+           // exists() - if exists in db record
+           if(DB::table('posts')->where('id', 343543)->exists()) {
+            echo 'Woohoo, I found a match';
+        } else {
+            echo 'Ahh, I have not found a match';
+        } 
 
-         // orWhereNot()
-         DB::table('posts')
-         ->where('min_to_read', '>', 5)
-         ->orWhereNot('is_published', true)
-         ->get();
+           // doesntExist() - if does not exists in db record
+           if(DB::table('posts')->where('id', 343543)->doesntExist()) {
+            echo 'Woohoo, I found a match';
+        } else {
+            echo 'Ahh, I have not found a match';
+        }
     }
 
     /**
