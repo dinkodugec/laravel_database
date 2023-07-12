70e8a0df3f2ad24a0168b4a7472470d8ac629fab
Dinko <dinko.dugec@gmail.com>
2023-07-12 23:17:05 +0200

    latest,oldest, orderBy

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,35 +13,14 @@ class PostController extends Controller
     public function index()
     {
       $posts =  DB::table('posts')
-      
-        ->selectRaw('count(*) as post_count')
-        ->first();
+      ->orderBy('title', 'desc')
+      ->orderBy('min_to_read')
+      ->get();
 
-    
-
-         // lazilyById()
-        // whereRaw()
-        DB::table('posts')
-            ->whereRaw('created_at > NOW() - INTERVAL 1 DAY')
-            ->get();
-
-        // havingRaw()
-        DB::table('posts')
-            ->select('user_id', DB::raw('SUM(min_to_read) as total_time'))
-            ->groupBy('user_id')
-            ->havingRaw('SUM(min_to_read) > 10')
-            ->get();
-
-        // orderByRaw()
-        DB::table('posts')
-            ->orderByRaw('created_at DESC')
-            ->get();
-
-        // groupByRaw()
-        DB::table('posts')
-            ->select('user_id', DB::raw('AVG(rating) as avg_rating'))
-            ->groupByRaw('user_id')
-            ->get();
+      $posts =  DB::table('posts')
+      ->latest('title')
+      ->oldest('title')
+      ->get();
     }
 
     /**
